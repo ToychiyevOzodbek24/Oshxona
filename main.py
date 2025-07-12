d8414b02fbcdcfb6c1cdf47652d7a1e2b89247a7
ToychiyevOzodbek24 <tuychiyevozodbek2@icloud.com>
2025-07-12 12:04:03 +0500

    main qushildi

---
+++ main.py
@@ -0,0 +1,12 @@
+git config --global user.name "ToychiyevOzodbek24"
+git config --global user.email "tuychiyevozodbek2@icloud.com"
+
+git branch -M main
+git remote add origin https://github.com/ToychiyevOzodbek24/Oshxona.git
+git push -u origin main
+
+
+
+
+
+
